4f1a9f598b9a072810742acd523c481071532428
Jaye Popplewell <jlpopp01@gmail.com>
2019-06-26 19:39:01 -0400

    Set up other class

---
+++ AdoptableArtSpeed/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdoptableArtSpeed
+{
+    class Art
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Nationality { get; set; }
+        public string Technique { get; set; }
+        public int Cost { get; set; }
+        public bool Adoptable { get; set; }
+    }
+}
